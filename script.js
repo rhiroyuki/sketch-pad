7223babaf8a785aade4a027d1a32bc3a72efb13e
rhiroyuki <ricardo.hiroyuki@hotmail.com>
2016-07-15 14:44:33 -0300

    Adiciona funcionalidade opcional

--- script.js
+++ script.js
@@ -1,35 +1,71 @@
 $(document).ready(function()
 {
-	$("body").append('<input id="clickMe" type="button" value="New" onclick="button_press();" />');
+	$("body").append('<input id="clickMe" type="button" value="New" onclick="button_press(); return false" />');
 	new_grid(16);
 });
 
 function button_press()
 {
-	$(".cell").css("background", "white");
-	var teste = prompt("How many squares per side to make the new grid?");
-	new_grid(teste);
+	$(".cell").css({
+		"background": "white"}
+	);
+	var square_quantity = prompt("How many squares per side to make the new grid?");
+	new_grid(square_quantity);
 }
 
-function new_grid(teste)
+function new_grid(square_quantity)
 {
 	$("#wrapper").remove();
-	$("body").append('<table id="wrapper"></table>');
+	$("body").append('<div id="wrapper"></div>');
 	var endrow = "";
-	for(var i=0;i<teste;i++)
+	for(var i=0;i<square_quantity;i++)
 	{
-		$("#wrapper").append('<tr id=' + i + ' class="row"></tr>');
-		for(var j=0;j<teste;j++)
+		$("#wrapper").append('<div id=' + i + ' class="row"></div>');
+		for(var j=0;j<square_quantity;j++)
 		{
-			$(".row#"+i).append('<td class="cell"></td>');
+			$(".row#"+i).append('<div class="cell"></div>');
 		}
 	}
-	var size = 930/teste;
-	$(".row").css({"border":"1px solid black","display":"inline-block", "width" : "930x"});
-	$(".cell").css({"border":"1px solid black", "height" : ""+size, "width" : ""+size, "padding":"0px 0px 0px 0px"});
-	$("#wrapper").css({"width":"1000px"});
+	var size = ((960)/square_quantity);
+	$(".row").css(
+		{"display":"inline-block"}
+	);
+	$(".cell").css(
+		{"border":"1px solid grey", 
+		"height" : ""+(size-2), 
+		"width" : ""+(size-2),
+		"background":"rgb(255,255,255)"}
+	);
+
 	$(".cell").mouseenter(function(){
-		$(this).css("background", "black");
+		var stringList = $(this).css("background").split(",");
+		
+		var r = parseInt(stringList[0].substring(4));
+		if(isNaN(r))
+		{
+			r = parseInt(stringList[0].substring(5));
+		}
+		var g = parseInt(stringList[1]);
+		var b = parseInt(stringList[2]);
+		if(r+g+b == 255+255+255)
+		{
+			$(this).css(
+			{"background":"rgb("+Math.floor(Math.random()*255 + 1) + ","
+							    +Math.floor(Math.random()*255 + 1) + ","
+				                +Math.floor(Math.random()*255 + 1) + ")"}
+			);
+		}
+		else
+		{ 
+			r-=25;
+			g-=25;
+			b-=25;
+			if(r<0) r=0;
+			if(g<0) g=0;
+			if(b<0) b=0;
+			$(this).css({"background":"rgb("+ r +","+ g +","+ b +")"}
+		);
+		}
 	})
 };
 
