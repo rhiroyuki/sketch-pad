7ee74980ce7f9c425805c364c5c053b862989c0d
rhiroyuki <ricardo.hiroyuki@hotmail.com>
2016-07-15 15:44:48 -0300

    Corrigido incompatibilidade do .css com shorthand properties

--- script.js
+++ script.js
@@ -1,13 +1,54 @@
 $(document).ready(function()
 {
-	$("body").append('<input id="clickMe" type="button" value="New" onclick="button_press(); return false" />');
-	new_grid(16);
+	$("body").append('<input id="clickMe" type="button" value="New" onclick="button_press(); return false"/>');
+	new_grid(16);	
+	var size = ((960)/16);
+	$(".row").css(
+		{"display":"inline-block"}
+	);
+	$(".cell").css(
+		{"border":"1px solid grey", 
+		"height" : ""+(size-2),
+		"width" : ""+(size-2),
+		"background-color":"rgb(255,255,255)"}
+	);
+	$(".cell").mouseenter(function(){
+		var stringList = ($(this).css("background-color")).split(",");
+		
+		var r = parseInt(stringList[0].substring(4));
+		if(isNaN(r))
+		{
+			r = parseInt(stringList[0].substring(5));
+		}
+		var g = parseInt(stringList[1]);
+		var b = parseInt(stringList[2]);
+		if(r+g+b == 255+255+255)
+		{
+			$(this).css(
+			{"background-color":"rgb("+Math.floor(Math.random()*255 + 1) + ","
+							    +Math.floor(Math.random()*255 + 1) + ","
+				                +Math.floor(Math.random()*255 + 1) + ")"}
+			);
+		}
+		else
+		{ 
+			r-=25;
+			g-=25;
+			b-=25;
+			if(r<0) r=0;
+			if(g<0) g=0;
+			if(b<0) b=0;
+			
+			$(this).css({"background-color":"rgb(" + r +"," + g + "," + b+ ")"}
+		);
+		}
+	})
 });
 
 function button_press()
 {
 	$(".cell").css({
-		"background": "white"}
+		"background-color": "white"}
 	);
 	var square_quantity = prompt("How many squares per side to make the new grid?");
 	new_grid(square_quantity);
@@ -32,41 +73,10 @@ function new_grid(square_quantity)
 	);
 	$(".cell").css(
 		{"border":"1px solid grey", 
-		"height" : ""+(size-2), 
+		"height" : ""+(size-2),
 		"width" : ""+(size-2),
-		"background":"rgb(255,255,255)"}
+		"background-color":"rgb(255,255,255)"}
 	);
-
-	$(".cell").mouseenter(function(){
-		var stringList = $(this).css("background").split(",");
-		
-		var r = parseInt(stringList[0].substring(4));
-		if(isNaN(r))
-		{
-			r = parseInt(stringList[0].substring(5));
-		}
-		var g = parseInt(stringList[1]);
-		var b = parseInt(stringList[2]);
-		if(r+g+b == 255+255+255)
-		{
-			$(this).css(
-			{"background":"rgb("+Math.floor(Math.random()*255 + 1) + ","
-							    +Math.floor(Math.random()*255 + 1) + ","
-				                +Math.floor(Math.random()*255 + 1) + ")"}
-			);
-		}
-		else
-		{ 
-			r-=25;
-			g-=25;
-			b-=25;
-			if(r<0) r=0;
-			if(g<0) g=0;
-			if(b<0) b=0;
-			$(this).css({"background":"rgb("+ r +","+ g +","+ b +")"}
-		);
-		}
-	})
 };
 
 
